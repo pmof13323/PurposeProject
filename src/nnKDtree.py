cdb9735580a1e1402cc5ced6d18e5c76ced3ed6e
OscarElliott <a1827086@student.adeliade.edu.au>
2024-02-12 23:36:50 +1030

    Removed Unused Spacing

--- src/nnKDtree.py
+++ src/nnKDtree.py
@@ -76,5 +76,8 @@ if __name__ == '__main__':
     # some sort of loop which runs search with given querry when request is recived
 
         # use User_Metric_Data as a querry point and run a NN Search
-
+        
         # display / send result 
+    
+    
+    
